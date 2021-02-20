62879eea75f99dc6ed2de45c1229ea7c5d5fbc0e
Aleksandr Akhapkin <sashapow1@gmail.com>
2021-02-21 02:36:41 +0300

    add ErrorDataIsInvalid

--- task4/pkg/infrastruct/error.go
+++ task4/pkg/infrastruct/error.go
@@ -22,4 +22,5 @@ var (
 	ErrorInternalServerError = NewError("Внутренняя ошибка сервера", http.StatusInternalServerError)
 	ErrorBadRequest          = NewError("Плохие входные данные", http.StatusBadRequest)
 	ErrorUserNotFound        = NewError("Результаты по вашему запросу не найдены", http.StatusOK)
+	ErrorDataIsInvalid       = NewError("Некорректная дата, формата ввода даты рождения: \"2020-12-30\"", http.StatusBadRequest)
 )
