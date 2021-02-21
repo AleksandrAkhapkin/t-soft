c85e443fd7903e08f568a2f42f8e7136fa17f89a
Aleksandr Akhapkin <sashapow1@gmail.com>
2021-02-21 03:04:54 +0300

    add NewRegister message

--- task4/internal/task4/types/types.go
+++ task4/internal/task4/types/types.go
@@ -1,9 +1,14 @@
 package types
 
 type User struct {
-	ID       int    `json:"id"`
+	ID       int64  `json:"id"`
 	Name     string `json:"name"`
 	Birthday string `json:"birthday"`
 	IsMale   bool   `json:"isMale"`
 	Age      int    `json:"age"`
+}
+
+type NewRegister struct {
+	Message string `json:"message"`
+	ID      int64  `json:"id"`
 }
