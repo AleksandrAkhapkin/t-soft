75b8fc41421c9d26f70c521094264b38ede79e48
Aleksandr Akhapkin <sashapow1@gmail.com>
2021-02-21 03:32:16 +0300

    hotfix

--- task4/pkg/infrastruct/error.go
+++ task4/pkg/infrastruct/error.go
@@ -21,6 +21,6 @@ func (c *CustomError) Error() string {
 var (
 	ErrorInternalServerError = NewError("Внутренняя ошибка сервера", http.StatusInternalServerError)
 	ErrorBadRequest          = NewError("Плохие входные данные", http.StatusBadRequest)
-	ErrorUserNotFound        = NewError("Результаты по вашему запросу не найдены", http.StatusOK)
+	ErrorUserNotFound        = NewError("Пользователь по вашему запросу не найден", http.StatusOK)
 	ErrorDataIsInvalid       = NewError("Некорректная дата, формата ввода даты рождения: \"2020-12-30\"", http.StatusBadRequest)
 )
